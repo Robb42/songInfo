b1f8e6a16660f17d7c0be6eea6b235fc9fbf0fae
Rob Bertini <rob.bertini@gmail.com>
2018-06-12 21:52:03 -0400

    created basic html and float-grid

---
+++ index.js
@@ -0,0 +1,56 @@
+/* Results HTML to print:
+
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>Lyrics</h2>
+
+          </div>          
+        </div>
+      </div>
+      
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>Meaning</h2>
+
+          </div>          
+        </div>
+      </div>
+      
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>Watch</h2>
+
+          </div>          
+        </div>
+      </div> 
+      
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>Reviews</h2>
+
+          </div>          
+        </div>
+      </div>
+      
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>Artist Info</h2>
+
+          </div>          
+        </div>
+      </div> 
+      
+      <div class="col-4">
+        <div>
+          <div>
+            <h2>On Tour?</h2>
+
+          </div>          
+        </div>
+      </div>   
+*/
