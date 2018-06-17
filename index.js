e639cfca9e417b2db778b8529adcbeffec37a3ad
Rob Bertini <rob.bertini@gmail.com>
2018-06-17 19:52:25 -0400

    progressing. using musicmatch api

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
 "use strict"
 
 const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
-const LYRICS_URL = "";
+const LYRICS_URL = "http://api.musixmatch.com/ws/1.1/";
 const SONGFACTS_URL = "";
 const REVIEWS_URL = "";
-const WIKI_URL = "";
+const WIKI_URL = "https://en.wikipedia.org/w/api.php";
 const TOURS_URL = "";
 
 function getDatafromApi(searchTerm, callback) {
   const queryYoutube = {
     part: "snippet",
     key: "AIzaSyBZcoFcX2hjtIRehNbhQyocEprrx2cOAfM",
-    q: `${searchTerm}`,
-    maxResults: "5",
+    q: `${searchTerm} song`,
+    maxResults: "3",
     type: "video",
     order: "viewCount",
     nextPageToken: "next",
     prevPageToken: "prev"
   };
-  const queryLyrics = {};
+  $.getJSON(YOUTUBE_URL, queryYoutube, callback);
+
+  const queryLyrics = {
+    apikey: "723d40ea5e7c1abaf24147f2d427939a",
+    q_track: `${searchTerm}`,
+    s_track_rating: "DESC",
+  };
+  //$.getJSON(LYRICS_URL, queryLyrics, callback);
+
   const querySongfacts = {};
   const queryReviews = {};
   const queryWiki = {};
   const queryTours = {};
-  $.getJSON(YOUTUBE_URL, queryYoutube, callback);
 }
 
 function renderResult(result) {
   let youtubeThumb = result.snippet.thumbnails.medium.url;
   let youtubeTitle = result.snippet.title;
   let youtubeID = result.id.videoId;
-  let youtubeChannel = result.snippet.channelId;
   let youtubeImgAlt = result.snippet.description;
+
+  //let lyricsBody = result.body.lyrics.lyrics_body;
+
   return `
+  <a href="https://www.youtube.com/watch?v=${youtubeID}"><img src="${youtubeThumb}" alt="${youtubeImgAlt}"></a>";
+  `
+}
+
+function displaySearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  console.log(results);
+  $("#results").prop("hidden", false).html(results);
+}
+
+function watchSubmit() {
+  $("#js-search-form").submit(function() {
+    event.preventDefault();
+    const query = $("#song-search").val();
+    console.log(query);
+    getDatafromApi(query, displaySearchData);
+  });
+}
+
+$(watchSubmit);
+
+/* `
         <div class="col-4">
         <div class="searchContainer">
           <div>
@@ -56,7 +87,6 @@ function renderResult(result) {
           <div>
             <h2>Watch</h2>
             <a href="https://www.youtube.com/watch?v=${youtubeID}"><img src="${youtubeThumb}" alt="${youtubeImgAlt}"></a>
-            <p><a href="https://www.youtube.com/channel/${youtubeChannel}">Click here</a> for more videos from this channel </p>
           </div>          
         </div>
       </div> 
@@ -87,21 +117,4 @@ function renderResult(result) {
           </div>          
         </div>
       </div>  
-      `
-}
-
-function displaySearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
-  $("#results").prop("hidden", false).html(results);
-}
-
-function watchSubmit() {
-  $("#js-search-form").submit(function() {
-    event.preventDefault();
-    const query = $("#song-search").val();
-    console.log(query);
-    getDatafromApi(query, displaySearchData);
-  });
-}
-
-$(watchSubmit);
+      `*/
