7344513af1e1914f18bf396dcb2e57d718774c2e
Rob Bertini <rob.bertini@gmail.com>
2018-06-17 22:04:08 -0400

    wiki and youtube apis working.  lyrics still cors errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict"
 
 const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
-const LYRICS_URL = "http://api.musixmatch.com/ws/1.1/";
+const LYRICS_URL = "https://api.musixmatch.com/ws/1.1/";
 const SONGFACTS_URL = "";
 const REVIEWS_URL = "";
 const WIKI_URL = "https://en.wikipedia.org/w/api.php";
 const TOURS_URL = "";
 
 function getDatafromApi(searchTerm, callback) {
-  const queryYoutube = {
+  /*const queryYoutube = {
     part: "snippet",
     key: "AIzaSyBZcoFcX2hjtIRehNbhQyocEprrx2cOAfM",
     q: `${searchTerm} song`,
@@ -17,37 +17,53 @@ function getDatafromApi(searchTerm, callback) {
     order: "viewCount",
     nextPageToken: "next",
     prevPageToken: "prev"
-  };
-  $.getJSON(YOUTUBE_URL, queryYoutube, callback);
+  };*/
+  //$.getJSON(YOUTUBE_URL, queryYoutube, callback);
 
   const queryLyrics = {
     apikey: "723d40ea5e7c1abaf24147f2d427939a",
     q_track: `${searchTerm}`,
     s_track_rating: "DESC",
+    origin: "*"
   };
-  //$.getJSON(LYRICS_URL, queryLyrics, callback);
+  $.getJSON(LYRICS_URL, queryLyrics, callback);
 
   const querySongfacts = {};
   const queryReviews = {};
-  const queryWiki = {};
+
+  const queryWiki = {
+    action: "query",
+    list: "search",
+    srsearch: `${searchTerm} song`,
+    format: "json",
+    origin: "*"
+  };
+  $.getJSON(WIKI_URL, queryWiki, callback);
+
   const queryTours = {};
 }
 
 function renderResult(result) {
-  let youtubeThumb = result.snippet.thumbnails.medium.url;
-  let youtubeTitle = result.snippet.title;
-  let youtubeID = result.id.videoId;
-  let youtubeImgAlt = result.snippet.description;
+  //let youtubeThumb = result.snippet.thumbnails.medium.url;
+  //let youtubeTitle = result.snippet.title;
+  //let youtubeID = result.id.videoId;
+  //let youtubeImgAlt = result.snippet.description;
 
-  //let lyricsBody = result.body.lyrics.lyrics_body;
+  //let trackId = search.result[0].pageid;
+  
+  //let wikiInfo = result.snippet; 
 
   return `
-  <a href="https://www.youtube.com/watch?v=${youtubeID}"><img src="${youtubeThumb}" alt="${youtubeImgAlt}"></a>";
+  <p>${lyricsBody}</p>
   `
 }
 
 function displaySearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
+  //const results = data.items.map((item, index) => renderResult(item));
+  console.log(data);
+  const results = data.query.map((item, index) => renderResult(item));
+  
+  //const results = data.query.search.map((item, index) => renderResult(item)); 
   console.log(results);
   $("#results").prop("hidden", false).html(results);
 }
